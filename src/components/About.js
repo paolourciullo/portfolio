efaf5d0539bda16735ae0dcbb476900668d8144e
Paolo U <paolourciullo@gmail.com>
2021-07-02 18:08:09 -0400

    fix that typo

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Paolo.
-            <br className="hidden lg:inline-block" />I am a Software Engineer
+            <br className="hidden lg:inline-block" />I'm a Software Engineer
           </h1>
           <p className="mb-8 leading-relaxed">
           I am a solution-oriented engineer who is always striving for the optimization of the task at hand. The delicate balance of quality and OTD is something I am familiar with. Using the agile methodology to implement features along with various Git workflows and tools, I am able to deliver the best possible product. 
