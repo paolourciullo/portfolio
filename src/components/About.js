90b3a9954ec3b3c26bdce307334d5b8eb454dabf
Paolo U <paolourciullo@gmail.com>
2021-07-02 16:20:33 -0400

    add picture

--- src/components/About.js
+++ src/components/About.js
@@ -32,7 +32,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src="./cartoonpic1.png"
           />
         </div>
       </div>
