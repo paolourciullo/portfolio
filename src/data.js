020280a650cbd159d9afbff2dd570eeae30a4486
Paolo U <paolourciullo@gmail.com>
2021-07-02 21:49:52 -0400

    add ext data

--- src/data.js
+++ src/data.js
@@ -1,60 +1,62 @@
 export const projects = [
   {
-    title: "Quiz App",
-    subtitle: "JavaScript",
+    title: 'Quiz App',
+    subtitle: 'JavaScript',
     description:
       "This app is a quiz app built with HTML5, CSS & Javascript.  It cycles through ten questions and reports the user's score at the end, all without a page reload!",
-    image: "./quizappcover.png",
-    link: "https://paolourciullo.github.io/quizapp/",
+    image: './quizappcover.png',
+    link: 'https://paolourciullo.github.io/quizapp/',
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: 'Movie App',
+    subtitle: 'JavaScript',
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "I used themoviedb.org's API to display a list of the current box office hits.  ES6 arrow functions check what the rating is and apply the appropiate color on render.",
+    image: './movieappcover.png',
+    link: 'https://paolourciullo.github.io/movieapp/',
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: 'DevChat',
+    subtitle: 'React and Firebase',
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-3.gif',
+    link: 'https://reactbootcamp.com',
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: 'Epic Todo App',
+    subtitle: 'React Hooks',
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-4.gif',
+    link: 'https://reactbootcamp.com',
   },
-];
+]
 
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+    image: 'https://randomuser.me/api/portraits/women/3.jpg',
+    name: 'Forkie Leghume',
+    company: 'Sporf.io',
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+    image: 'https://randomuser.me/api/portraits/men/90.jpg',
+    name: 'Stonch Filipilipilipilios',
+    company: 'Napple.io',
   },
-];
+]
 
 export const skills = [
-  "JavaScript",
-  "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
-];
+  'JavaScript',
+  'React',
+  'Next.js',
+  'Node',
+  'Python',
+  'Figma',
+  'Git',
+  'Agile',
+]
